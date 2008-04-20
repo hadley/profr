4199571d8e338d988a95c37e8dd949fceee0bfb2
hadley <h.wickham@gmail.com>
2008-04-20 10:31:54 -0500

    Basic project scaffolding

---
+++ load.r
@@ -0,0 +1,11 @@
+# library(prof)
+
+FILE <- (function() {
+  attr(body(sys.function()), "srcfile")
+})()$filename
+PATH <- dirname(FILE)
+
+source.with.err <- function(path) {
+  tryCatch(source(path), error = function(x) {print(path); print(x)})
+}
+lapply(dir(file.path(PATH, "R"), full.name=T), source.with.err)
